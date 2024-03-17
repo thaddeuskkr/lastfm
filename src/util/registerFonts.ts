d484d2d277e52d514f5ec709c39019bc9e37d5eb
Thaddeus Kuah <tk@tkkr.dev>
2024-03-17 09:09:01 +0000

    chore: remove light fonts
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/util/registerFonts.ts
+++ src/util/registerFonts.ts
@@ -3,10 +3,8 @@ import { registerFont } from 'canvas';
 export default function registerFonts() {
     registerFont('assets/fonts/SourceHanSans-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSans-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
-    registerFont('assets/fonts/SourceHanSans-Light.otf', { family: 'Source Han Sans', weight: 'light' });
     registerFont('assets/fonts/SourceHanSansHC-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSansHC-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
-    registerFont('assets/fonts/SourceHanSansHC-Light.otf', { family: 'Source Han Sans', weight: 'light' });
     registerFont('assets/fonts/SourceHanSansHW-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSansHW-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
     registerFont('assets/fonts/SourceHanSansHWHC-Regular.otf', { family: 'Source Han Sans' });
@@ -19,11 +17,8 @@ export default function registerFonts() {
     registerFont('assets/fonts/SourceHanSansHWTC-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
     registerFont('assets/fonts/SourceHanSansK-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSansK-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
-    registerFont('assets/fonts/SourceHanSansK-Light.otf', { family: 'Source Han Sans', weight: 'light' });
     registerFont('assets/fonts/SourceHanSansSC-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSansSC-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
-    registerFont('assets/fonts/SourceHanSansSC-Light.otf', { family: 'Source Han Sans', weight: 'light' });
     registerFont('assets/fonts/SourceHanSansTC-Regular.otf', { family: 'Source Han Sans' });
     registerFont('assets/fonts/SourceHanSansTC-Bold.otf', { family: 'Source Han Sans', weight: 'bold' });
-    registerFont('assets/fonts/SourceHanSansTC-Light.otf', { family: 'Source Han Sans', weight: 'light' });
 }
